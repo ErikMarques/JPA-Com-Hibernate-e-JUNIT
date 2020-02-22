d3da137732a5992ee2551556fc92d5ce5050ad00
Erik Marques <erikmsilva@gmail.com>
2020-02-22 12:55:06 -0300

    Criando entidade e tabelas

---
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -0,0 +1,13 @@
+package posjavamavenhibernate;
+
+import org.junit.Test;
+
+public class TesteHibernate {
+
+	@Test
+	public void testeHibernateUtil() {
+		//System.out.println("Projeto Maven - Hibernate Rodando!");
+		HibernateUtil.geEntityManager();
+	}
+}	
+
